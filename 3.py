907af5ffb48458cf6ff7f9123ffe5788c88dece2
Michael1876 <131909319+Michael1876@users.noreply.github.com>
2026-04-20 00:47:59 +0300

    Update 3.py
    8

--- 3.py
+++ 3.py
@@ -1,16 +1,12 @@
-try:
-    a = float(input("Число 1: "))
-    b = float(input("Число 2: "))
-    op = input("Операция (+, -, *, /): ").strip()
+a = float(input("Число 1: "))
+b = float(input("Число 2: "))
+op = input("Операция (+, -, *, /): ").strip()
 
-    ops = {
-        '+': a + b, 
-        '-': a - b, 
-        '*': a * b, 
-        '/': a / b if b != 0 else "Ошибка: /0"
-    }
+ops = {
+    '+': a + b, 
+    '-': a - b, 
+    '*': a * b, 
+    '/': a / b if b != 0 else "Ошибка: /0"
+}
 
-    print(f"Результат: {ops.get(op, 'Неверный оператор')}")
-
-except ValueError:
-    print("Ошибка: введите числа")
+print(f"Результат: {ops.get(op, 'Неверный оператор')}")
